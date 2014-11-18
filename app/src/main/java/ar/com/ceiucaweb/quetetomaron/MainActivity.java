234d24283705accf030c3ec20317b0a09a7b27de
Juan Manuel Mougan <juanmougan@gmail.com>
2014-11-18 20:00:24 -0300

    Refactor: se extrae el PlaceholderFragment. Se va a transformar en los Fragments de Explorar y Cargar

--- app/src/main/java/ar/com/ceiucaweb/quetetomaron/MainActivity.java
+++ app/src/main/java/ar/com/ceiucaweb/quetetomaron/MainActivity.java
@@ -10,13 +10,10 @@ import android.app.FragmentTransaction;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
+import ar.com.ceiucaweb.quetetomaron.fragment.PlaceholderFragment;
 
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
@@ -125,17 +122,20 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+            // TODO - esto se cambia por el Fragment que sea (explorar, subir)
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
+            // TODO - la cantidad de Fragments que haya (explorar, subir, etc?)
             return 3;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
+            // TODO - el título del Fragment va a ser un atributo del Fragment
             Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
@@ -146,39 +146,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
                     return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
         }
     }
 
